596e26cb5f256ebd92164482bb6188de2021f730
Rodrigo Almeida <rodrigo.almeida@sinqia.com.br>
2020-08-23 20:58:43 -0300

    make it better

---
+++ src/main/java/com/cadastro/apirest/models/Cadastro.java
@@ -0,0 +1,67 @@
+package com.cadastro.apirest.models;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+
+@Entity
+@Table(name="TB_CADASTRO")
+public class Cadastro implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long id;
+	
+	@NotNull
+	private String Nome;
+	
+	@NotNull
+	private String CPF;
+	
+	@NotNull
+	private String endereco;
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getCPF() {
+		return CPF;
+	}
+
+	public void setCPF(String cPF) {
+		CPF = cPF;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getNome() {
+		return Nome;
+	}
+
+	public void setNome(String nome) {
+		Nome = nome;
+	}
+
+	
+
+}
